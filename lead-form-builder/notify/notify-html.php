b19675ea1bf1a627326643c2310c3314305d939d
Sean Murphy <sean@iamseanmurphy.com>
2025-07-05 11:17:46 +0000

    2.0.0

--- lead-form-builder/notify/notify-html.php
+++ lead-form-builder/notify/notify-html.php
@@ -1,42 +1,41 @@
-<div class="notice th-notice-slide-wrapper" style="display:<?php echo $display; ?>;">
- <div class="th-notice-slide-wrap">
     <?php 
     if($theme !== 'th-shop-mania'){ ?>
+  <div class="notice th-notice-slide-wrapper" style="display:<?php echo esc_html($display); ?>;">
+ <div class="th-notice-slide-wrap owl-carousel">
+
 
                 <div class="thunk-notice">
                     <div class="thunk-review-thumbnail">
-                        <a href="https://themehunk.com/th-shop-mania/"
+                        <a href="<?php echo esc_url('https://themehunk.com/th-shop-mania/');?>"
                            target="_blank">
                             <img src="<?php echo LFB_PLUGIN_URL; ?>notify/assets/images/th-shop-mania.png"
-                                 alt="Th Shop Mania: Free WooCommerce WordPress theme"/>
+                                 alt="<?php _e('TH Shop Mania: Free WooCommerce WordPress theme','lead-form-builder'); ?>"/>
                         </a>
                     </div>
                     <div class="thunk-notice-text">
-                        <h3>Th Shop Mania: Free WooCommerce WordPress theme</h3>  
+                        <h3><?php _e('TH Shop Mania: Free WooCommerce WordPress Theme','lead-form-builder'); ?></h3>  
                         <a href="?notice-disable=1"  class="notice-dismiss dashicons dashicons-dismiss dashicons-dismiss-icon"></a>
-                        <p>Elementor and WooCommerce combo make this theme perfect for shop with full editing, We also offer ready to import shop websites, just download it and start tuning, Everything is free .
+                         <p><?php _e('Elementor and WooCommerce combo make this theme perfect for shop with full editing, We also offer ready to import shop websites, just download it and start tuning, Everything is free.','lead-form-builder')?>
                         </p>
                         <ul class="thunk-review-ul">
                             <li class="show-mor-message">
-                                <a href="https://themehunk.com/th-shop-mania/#mania-demo"
+                                <a href="<?php echo esc_url('https://themehunk.com/th-shop-mania/#mania-demo');?>"
                                    target="_blank">
                                     <span class="dashicons dashicons-desktop"></span>
-                                    Live Demo
+                                   <?php _e('Live Demo','lead-form-builder');?>
                                 </a>
                             </li>
                             <li class="free-download-message">
-                                <a href="https://themehunk.com/th-shop-mania/"
+                                <a href="<?php echo esc_url('https://themehunk.com/th-shop-mania/');?>"
                                    target="_blank">
                                     <span class="dashicons dashicons-external"></span>
-                              Know More
+                              <?php _e('Check Detail','lead-form-builder'); ?>
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </div>  <!--end-->
 
-              <?php } ?>
-
-
-     </div>
-</div>
+            </div>
+             </div>
+                           <?php } ?>
